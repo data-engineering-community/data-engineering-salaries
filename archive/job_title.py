5b1cb8b6b14a913ee3d27a044a6d31e459af71f2
Apurva Hegde <ahegde@Apurvas-Laptop.local>
2023-10-26 15:39:46 -0500

    modularized the job title script and ran it on historical data

--- archive/job_title.py
+++ archive/job_title.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 
 def extract_job_title(body_df, colIdx, colname):
-    
+    """
+    Takes the body of the comments in the form of a pandas df and the column index of column containing job title info, 
+    renames the column to the "colname" variable, and returns job title information. body_df, colIdx, colname are all required variables.
+    """
     ######### EXTRACT JOB TITLE
 
     #standardize title column name
@@ -13,15 +16,9 @@ def extract_job_title(body_df, colIdx, colname):
     filter_condition = ~body_df[colname].str.lower().str.startswith(tuple(pattern.lower() for pattern in patterns))
     body_df.loc[filter_condition] = ""
 
-    # transformed_filtered=body_df.loc[filter_condition]
-    # # transformed_filtered.to_csv(path_or_buf="./archive/test_filtered.csv", )
-    # print(transformed_filtered.shape)
-
     #remove numbers like "1.", etc. from the beginning of the string
     body_df[colname] = body_df[colname].str.replace("^[ ]?1[.:)][ ]?","",regex=True)
     body_df[colname] = body_df[colname].str.lower().str.replace("^[ ]?(current)?[ ]?(title)?[ ]?[:]?[ ]","",regex=True)
-    #transformed_filtered[colname] = transformed_filtered[colname].str.replace("⁠[ ]?title[:]?[ ]?","", regex=True)
-    #transformed_filtered.to_csv(path_or_buf="./archive/test_filtered.csv", )
 
     #correct some edge cases
     body_df[colname] = body_df[colname].str.replace(".*title:?\s?","", regex=True)
