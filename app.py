3941b081241269926b335f3aaff694458d28a7d8
Benjamin Earl <14208829+benjameep@users.noreply.github.com>
2024-01-05 14:24:44 -0700

    parsed the "Tech Stack" answers and added a filter for them

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import datetime
 
 import pandas as pd
+import numpy as np
+import re
 import streamlit as st
 import plotly.express as px
 
@@ -22,6 +24,76 @@ def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
+def parse_tech_stack(raw: pd.Series) -> pd.DataFrame:
+    REMOVE = ['etc','cloud','devops']
+    RENAME = {
+        'js': 'javascript',
+        'k8s': 'kubernetes',
+        'azurecloud': 'azure',
+        'golang': 'go',
+        'googlecloud': 'gcp',
+        'oci': 'oracle',
+        'postgressql': 'postgres',
+        'postgresql': 'postgres',
+        'sqlserver': 'mssql',
+        'iics': 'informatica',
+        'synapse': 'azuresynapse',
+        'glue': 'awsglue',
+        'lambda': 'awslambda',
+        'datafactory': 'azuredatafactory',
+        'bq': 'bigquery'
+    }
+    # remove nulls and strip whitespace
+    raw = raw.dropna().str.strip()
+    # split the string on commas and other punctuation
+    # clean up the whitespace for each segment and remove empty segments
+    # take a value count for each segment
+    segment_counts = raw.str.split(r'[,\\/\n\.()]', regex=True).explode()\
+        .str.strip()\
+        .replace(r'\s+',' ', regex=True)\
+        .replace('', np.nan)\
+        .dropna()\
+        .value_counts()\
+        .reset_index().set_axis(['raw','cnt'], axis=1)
+    # normalize segments by removing whitespace and capitalization
+    # so that "MongoDB" and "mongo db" will both become "mongodb"
+    # also use our RENAME map to combine acronyms and such
+    normalized = segment_counts.raw.str.lower().str.replace(r'\s+','', regex=True).replace(RENAME)
+    # combine the segments by that normalized form
+    g = segment_counts.groupby(normalized)
+    tokens = pd.DataFrame({
+        # Use the most common capitalization as the 'title'
+        'title': g.raw.apply(lambda n: list(n)[0]),
+        'aliases': g.raw.apply(list),
+        'cnt': g.cnt.sum(),
+    }).query('cnt > 1').drop(index=REMOVE, errors='ignore')
+    # Using the token table that we created, search for each alias in each response
+    # creating a wide table of boolean values.
+    # This will allow for being able to parse tech stack names out of responses
+    # that aren't nicely delimited. For example:
+    # 
+    # input:        | Tech Stack                        |
+    #         |-----|-----------------------------------|
+    #         |  1  | "We use SQL and sometimes Python" |
+    #         |  2  | "python, Azure"                   |
+    #
+    # output:       | SQL   | Python | Azure |
+    #         |-----|-------|--------|-------|
+    #         |  1  | True  | True   | False |
+    #         |  2  | False | True   | True  |
+    #
+    wide = pd.DataFrame(index=raw.index)
+    for _, token in tokens.iterrows():
+        wide[token.title] = False
+        for alias in token.aliases:
+            # I couldn't surround the tech name with `\b` because apparently 
+            # that doesn't work for things like "c#"
+            regex = r'(?:^|\s|[\.,()/\\])'\
+                + re.escape(alias) \
+                + r'(?:$|\s|[\.,()/\\])'
+            wide.loc[raw.str.contains(regex, regex=True), token.title] = True
+    return wide
+
 
 salaries_df = load_data(st.secrets["public_gsheets_url"])
 salaries_df = salaries_df[["Submitted at", "Current Job Title", "Other", "Years of Experience", "City", "State", "Country", "Work Arrangement",
@@ -30,6 +102,7 @@ salaries_df.dropna(how="all", inplace=True)
 salaries_df['Submitted at'] = pd.to_datetime(
     salaries_df["Submitted at"]).dt.date
 salaries_df.sort_values(by=["Submitted at"], ascending=False, inplace=True)
+tech_stack_df = parse_tech_stack(salaries_df['Tech Stack'])
 min_yoe = int(salaries_df["Years of Experience"].min())
 max_yoe = int(salaries_df["Years of Experience"].max())
 today = datetime.date.today()
@@ -41,6 +114,7 @@ session_state_variables = [
     "filter_job_title",
     "filter_work_arrangement",
     "filter_industry",
+    "filter_tech",
     "filter_city",
     "filter_state",
     "filter_country",
@@ -67,6 +141,9 @@ job_title_list = [  # Empty string = no filter
 work_arrangement_list = ["", *list(salaries_df["Work Arrangement"].unique())]
 industry_list = [
     "", *sorted(list(salaries_df["Industry"].fillna("Unknown").unique()))]
+tech_stack_list = [
+    "", *tech_stack_df.sum().sort_values(ascending=False).index.tolist(),
+]
 currency_list = [*sorted(list(salaries_df["Currency"].unique()))]
 city_list = [
     "", *sorted(list(salaries_df["City"].fillna("Unknown").unique()))]
@@ -93,6 +170,10 @@ if st.session_state.filter_work_arrangement:
 if st.session_state.filter_industry:
     salaries_df = salaries_df.loc[
         salaries_df["Industry"] == st.session_state.filter_industry
+    ]
+if st.session_state.filter_tech:
+    salaries_df = salaries_df.loc[
+        salaries_df.join(tech_stack_df[st.session_state.filter_tech].rename('chosen_tech')).chosen_tech == True
     ]
 if st.session_state.filter_currency:
     salaries_df = salaries_df.loc[
@@ -170,6 +251,7 @@ with st.sidebar:
         step=1,
         key="filter_yoe",
     )
+    st.selectbox("Tech Stack", options=tech_stack_list, key="filter_tech")
     st.selectbox("Currency", options=currency_list, key="filter_currency")
     st.selectbox("City", options=city_list, key="filter_city")
     st.selectbox("State", options=state_list, key="filter_state")
